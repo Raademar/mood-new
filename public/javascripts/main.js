d393ec1034e123085cab731fc218d04cb11130ec
Mattias Rådemar <mattiasrademar@Mattiass-MacBook-Pro.local>
2018-09-29 09:03:12 +0200

    merge recent changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -116,4 +116,74 @@ closeButton.addEventListener('click', toggleModal);
 window.addEventListener('click', windowOnClick);
 
 
+// Typewriter functionality
+const TypeWriter = function(txtElement, words, wait = 3000) {
+  this.txtElement = txtElement
+  this.words = words
+  this.txt = ''
+  this.wordIndex = 0
+  this.wait = parseInt(wait, 10)
+  this.type()
+  this.isDeleting = false
+}
 
+// Type method
+TypeWriter.prototype.type = function() {
+  // Current index of word
+  const current = this.wordIndex % this.words.length
+  // Get full text of current word
+  const fullTxt = this.words[current]
+
+  // Check if deleting
+  if(this.isDeleting) {
+    // Delete a character
+    this.txt = fullTxt.substring(0, this.txt.length - 1)
+  } else {
+    // Add a character
+    this.txt = fullTxt.substring(0, this.txt.length + 1)
+  }
+
+  // Insert txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
+  const cursor = document.querySelector('.txt')
+  // Make cursor blink
+  function cursorBlinker() {
+    cursor.classList.toggle('hideCursor')
+  }
+
+  // Initial Type speed
+  let typeSpeed = 300
+
+  if(this.isDeleting) {
+    typeSpeed /= 2
+  }
+
+  // Check if word is complete
+  if(!this.isDeleting && this.txt === fullTxt) {
+    // Make a pause at the end of each word
+    typeSpeed = this.wait
+    //setInterval(cursorBlinker, 200)
+    // Set delete to true
+    this.isDeleting = true
+  } else if(this.isDeleting && this.txt === '') {
+    this.isDeleting = false
+    // Move on to the next word
+    this.wordIndex++
+    // Pause before new word starts typing
+    typeSpeed = 500
+    //setInterval(cursorBlinker, 200)
+  }
+  setTimeout(() => this.type(), typeSpeed)
+}
+
+// Init on DOM Load
+document.addEventListener('DOMContentLoaded', init)
+
+// Init app
+function init() {
+  const txtElement = document.querySelector('.txt-type')
+  const words = JSON.parse(txtElement.getAttribute('data-words'))
+  const wait = txtElement.getAttribute('data-wait')
+  // Init Typewriter
+  new TypeWriter(txtElement, words, wait)
+}
